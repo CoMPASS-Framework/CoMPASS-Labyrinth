1d9d6c6801f735f579dee2e2fd119ce2e29da8cc
luiztauffer <24541631+luiztauffer@users.noreply.github.com>
2025-10-09 19:26:56 +0200

    save figure utils

--- src/compass_labyrinth/utils.py
+++ src/compass_labyrinth/utils.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import yaml
+import os
 
 
 def load_project(project_path: Path | str) -> dict:
@@ -25,3 +26,36 @@ def load_project(project_path: Path | str) -> dict:
         config = yaml.safe_load(file)
 
     return config
+
+
+def save_figure(
+    config: dict,
+    fig_name: str,
+    subdir: str = 'results/task_performance',
+    dpi: int = 300,
+    ext: str = 'pdf'
+):
+    """
+    Save the current matplotlib figure to a standardized results folder.
+
+    Parameters
+    ----------
+    config : dict
+        Project's configuration dictionary.
+    fig_name : str
+        Name of the figure file, e.g., 'Shannons_entropy' or 'Bout_Success'.
+        Extension is automatically appended as defined by `ext`.
+    subdir : str
+        Subfolder path under BASE_PATH to save the figure.
+    dpi : int
+        Resolution of saved figure.
+    ext : str
+        File extension, e.g., 'pdf', 'png', etc.
+    """
+    import matplotlib.pyplot as plt
+    
+    base_path = config["project_path_full"]
+    os.makedirs(os.path.join(base_path, subdir), exist_ok=True)
+    save_path = os.path.join(base_path, subdir, f"{fig_name}.{ext}")
+    plt.savefig(save_path, bbox_inches='tight', dpi=dpi)
+    print(f"Saved: {save_path}")
